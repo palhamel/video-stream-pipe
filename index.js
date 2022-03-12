755b5289703c44905f87d6f61094ac4f45e3234b
Pål Hamel <pal.hamel@gmail.com>
2022-03-12 21:59:28 +0100

    add file path and change styling

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.listen(port, () => console.log(`Ready - app listening on http://localhost/${
 
 // serve html:
 app.get('/', (req, res) => {
-  console.log('> http request received');
+  console.log('> http request received')
   try {
     res.sendFile(__dirname + '/public/index.html')
   } catch (error) {
@@ -20,13 +20,18 @@ app.get('/', (req, res) => {
   }
 })
 
+// Movie API:
+// External movie
+
+
 app.get('/video', (req, res) => {
+  // console.log(typeof(extLink))
   // indicates the part of a document that the server should return
   // on this measure in bytes for example: range = 0-6 bytes.
   const range = req.headers.range
   if (!range) res.status(400).send('Range must be provided')
 
-  const videoPath = path.join(__dirname, 'public/video', 'video.mp4')
+  const videoPath = path.join(__dirname, 'public/video', 'BigBuckBunny.mp4')
   // extract video size by using statSyn()
   const videoSize = fs.statSync(videoPath).size
   // 10 powered by 6 equal 1000000bytes = 1mb
@@ -50,4 +55,13 @@ app.get('/video', (req, res) => {
 
   // create live stream pipe line
   videoStream.pipe(res)
+})
+
+// serving poster image;
+app.get('/:file_name', (req, res) => {
+  try {
+    res.sendFile(__dirname + '/public/images/' + req.params.file_name)
+  } catch (err) {
+    res.status(500).send('internal server error occurred')
+  }
 })
